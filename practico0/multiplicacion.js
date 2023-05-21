3b2a8db2d3f162e4005fe3c0788a0da6baca0dc6
maumaco <mauriciomargaritcornejo@gmail.com>
2023-05-21 13:35:04 -0300

    Agrego multiplicacion.js y resta.js

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,7 @@
+let factor1 = 2;
+let factor2 = 17;
+let factor3 = 6;
+let producto = factor1 * factor2 * factor3;
+console.log('Multiplicación');
+console.log('¿Qué valor devuelve el programa si los factores son 2, 17 y 6?');
+console.log(producto);
---
+++ practico0/resta.js
@@ -0,0 +1,6 @@
+let minuendo = 10;
+let sustraendo = 2;
+let diferencia = minuendo - sustraendo;
+console.log('Resta');
+console.log('¿Qué valor devuelve el programa si el minuendo es 10 y el sustraendo es 2?');
+console.log(diferencia);
