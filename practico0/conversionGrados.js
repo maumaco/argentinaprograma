86a95ed10ad7a6642d93a7ee278c92e5308948c8
maumaco <mauriciomargaritcornejo@gmail.com>
2023-05-21 13:38:17 -0300

    Agrego conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,10 @@
+let fahrenheit = 50;
+let celsius = (fahrenheit - 32) * 5 / 9;
+console.log('Conversión de grados Fahrenheit a grados Celsius');
+console.log('¿Cuántos grados Celsius son 50 grados Fahrenheit?');
+console.log(celsius);
+
+fahrenheit = 75;
+celsius = (fahrenheit - 32) * 5 / 9;
+console.log('¿Cuántos grados Celsius son 75 grados Fahrenheit?');
+console.log(celsius);
