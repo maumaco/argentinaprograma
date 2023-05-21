a67b1ac74629c83948f3ba1044ef8dbf09f13ac0
maumaco <mauriciomargaritcornejo@gmail.com>
2023-05-21 13:48:17 -0300

    Agrego areaRectangulo.js y promedio3.js. Práctico 0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,12 @@
+let base = 5;
+let altura = 3;
+let area = base * altura;
+console.log('Área de un rectángulo');
+console.log('¿Cuál es el área de un rectángulo de 5 cm de base y de 3 cm de altura?');
+console.log(area);
+
+base = 12.5;
+altura = 5.3;
+area = base * altura;
+console.log('¿Cuál es el área de un rectángulo de 12.5 cm de base y de 5.3 cm de altura?');
+console.log(area);
---
+++ practico0/promedio3.js
@@ -0,0 +1,21 @@
+let numero1 = 5;
+let numero2 = 7;
+let numero3 = 9;
+let promedio = (numero1 + numero2 + numero3) / 3;
+console.log('Promedio de tres números');
+console.log('¿Cuál es el promedio de 5, 7 y 9?');
+console.log(promedio);
+
+numero1 = -2;
+numero2 = -3;
+numero3 = -8;
+promedio = (numero1 + numero2 + numero3) / 3;
+console.log('¿Cuál es el promedio de -2, -3 y -8?');
+console.log(promedio);
+
+numero1 = -1.2;
+numero2 = 5.3;
+numero3 = -3.3;
+promedio = (numero1 + numero2 + numero3) / 3;
+console.log('¿Cuál es el promedio de -1.2, 5.3 y -3.3?');
+console.log(promedio);
